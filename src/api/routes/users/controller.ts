bb1dab8100e459a4e9a83b01f98b93e65d837cda
Reiter Baier <91735552+ReiterBaier@users.noreply.github.com>
2022-05-25 14:41:27 -0300

    Update controller.ts

--- src/api/routes/users/controller.ts
+++ src/api/routes/users/controller.ts
@@ -1,9 +1,9 @@
-import { JsonController, Post, Param, HttpCode, OnUndefined } from 'routing-controllers'
+import { JsonController, Post, Param, HttpCode, OnUndefined, Get } from 'routing-controllers'
 import { AuthUser, ResponseAuthUser } from './service'
 
 @JsonController('/suportfy')
 export class UserAuthentication {
-  @Post('/auth')
+  @Get('/auth')
   @HttpCode(200)
   @OnUndefined(400)
   postUser(@Param('user') user: string, @Param('password') password:string) {
