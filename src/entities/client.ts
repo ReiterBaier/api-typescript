44c501cc21684071812f8dfb61956db9e7b5fc8d
Reiter Baier <91735552+ReiterBaier@users.noreply.github.com>
2022-10-10 15:53:25 -0300

    Ajuste

--- src/entities/client.ts
+++ src/entities/client.ts
@@ -1,8 +1,8 @@
-import { Entity, Column, PrimaryColumn } from 'typeorm'
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity('client')
 export class Client {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn("uuid")
   id: number
 
   @Column()
--- src/migrations/1665427285778-CreateClient.ts
+++ src/migrations/1665427285778-CreateClient.ts
@@ -1,11 +1,36 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateClient1665427285778 implements MigrationInterface {
+export default class CreateClient1665427285778 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "client",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()"
+          },
+          {
+            name: "document",
+            type: "varchar(20)"
+          },
+          {
+            name: "name",
+            type: "varchar(100)"
+          },
+          {
+            name: "token",
+            type: "varchar(200)"
+          }
+        ]
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("client");
+  }
 }
