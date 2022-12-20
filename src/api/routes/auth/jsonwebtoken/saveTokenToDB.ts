9b6759663747cfad56132029f0958e3510198e2d
VictorTarnovski <victorcruztarnovski@gmail.com>
2022-12-20 15:20:23 -0300

    chore: changed dir path's

--- src/api/routes/auth/jsonwebtoken/saveTokenToDB.ts
+++ src/api/routes/auth/jsonwebtoken/saveTokenToDB.ts
@@ -1,5 +1,5 @@
-import { AppDatasource } from '../../config/databaseConnection'
-import { User } from '../../entities/user'
+import { AppDatasource } from '../../../../config/databaseConnection'
+import { User } from '../../../../entities/user'
 
 export const saveTokenToDB = (userID: number,token:any) => {
     AppDatasource
