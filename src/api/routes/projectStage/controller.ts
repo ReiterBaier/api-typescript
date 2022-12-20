f4b1a76ae821072bd4a48a41b5da5d855b964fdc
VictorTarnovski <victorcruztarnovski@gmail.com>
2022-12-20 16:25:47 -0300

    add token support to projectStage route

--- src/api/routes/projectStage/controller.ts
+++ src/api/routes/projectStage/controller.ts
@@ -1,12 +1,16 @@
-import { JsonController,Param, HttpCode, OnUndefined, Get} from 'routing-controllers'
+import { JsonController,HeaderParam, HttpCode, OnUndefined, Get} from 'routing-controllers'
 import { findAll } from './service'
+import { validateToken } from '../auth/jsonwebtoken/token-validator'
 
 @JsonController('/suportfy')
 export class getAllProjectStage {
   @Get('/projectStage')
   @HttpCode(200)
   @OnUndefined(400)
-  getAll() {
+  getAll(@HeaderParam('authentication') token: string) {
+    if (!validateToken(token)) {
+      return new Error('error: Your token is not valid or has already expired')
+    } else
     return findAll()
   }
 }
